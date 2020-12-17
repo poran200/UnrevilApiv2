3f8f4e5d19938c1d62d81f50022edfb1450a3271
Poran chowdury <chowdury659@gmail.com>
2020-12-17 14:28:37 +0600

    ReviewImage change

--- src/main/java/com/unriviel/api/model/metadata/review/ReviewImage.java
+++ src/main/java/com/unriviel/api/model/metadata/review/ReviewImage.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewImage implements Serializable {
+     private String url;
      private AnsStatus isRelated;
      private AnsStatus isGoodComposition;
 
--- src/test/java/com/unriviel/api/service/UserRepositoryTest.java
+++ src/test/java/com/unriviel/api/service/UserRepositoryTest.java
@@ -1,7 +1,8 @@
 package com.unriviel.api.service;
 
 import com.unriviel.api.repository.RoleRepository;
-import com.unriviel.api.repository.UserRepository;
+import com.unriviel.api.repository.UserPaginationRepertory;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,14 +10,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class UserRepositoryTest {
     @Autowired
-    UserRepository userRepository;
+    UserPaginationRepertory userRepository;
     @Autowired
     RoleRepository roleRepository;
 
-//
-//    @Test
-//    void findUserIsAdmin() {
+
+    @Test
+    void findUserIsAdmin() {
 //        var streamable = userRepository.findAll(PageRequest.of(0, 10)).filter(User::isAdmin);
 //        System.out.println("collect = " + streamable.toList());
-//    }
+//        var userPage = userRepository.findAllByUsernameStartingWithAndRolesRole("a",RoleName.ROLE_ADMIN , Pageable.unpaged());
+//        System.out.println("userPage = " + userPage.getContent().toString());
+    }
 }
