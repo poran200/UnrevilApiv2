716d918c2e39bdb73aa4a375a6b3b47deafc94d0
Poran chowdury <chowdury659@gmail.com>
2020-12-22 23:54:10 +0600

    video download module

--- src/main/java/com/unriviel/api/controller/VideoIdGenerateController.java
+++ src/main/java/com/unriviel/api/controller/VideoIdGenerateController.java
@@ -2,11 +2,13 @@ package com.unriviel.api.controller;
 
 import com.unriviel.api.annotation.APiController;
 import com.unriviel.api.dto.VideoResponse;
+import com.unriviel.api.model.CustomUserDetails;
 import com.unriviel.api.service.VideoMetaDataService;
 import com.unriviel.api.util.ContentType;
 import com.unriviel.api.util.UrlConstrains;
 import com.unriviel.api.util.VideoIdGenerator;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,14 +28,17 @@ public class VideoIdGenerateController {
 
     @GetMapping(UrlConstrains.VideoIdGenerate.GENERATE)
     public ResponseEntity<Object> generateId(@PathVariable(required = true) int numberOfId){
+        var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+         CustomUserDetails userDetails = (CustomUserDetails) principal;
+        var email = userDetails.getEmail();
 
-          Map<Integer, String> videosId  = new HashMap<>();
+        Map<Integer, String> videosId  = new HashMap<>();
           for (int i = 1; i<= numberOfId; i++){
               Random random = new Random();
               // generate a random integer from 0 to 899, then add 100
               int x = random.nextInt(900) + 100;
               var uuid = VideoIdGenerator.generateUUID(ContentType.VIDEO, x);
-              videoMetaDataService.saveVideoStatus(new VideoResponse(uuid,null,false,null));
+              videoMetaDataService.saveVideoStatus(new VideoResponse(uuid,null,false,email));
               videosId.put(i,uuid);
           }
         return ResponseEntity.ok().body(videosId);
